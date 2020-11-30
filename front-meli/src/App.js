7701653a2723729c9c8b0b45178202a4899abc66
Sebastian Demaria <sdemaria@MAC-502-IT6.fibertel.com.ar>
2020-11-29 21:52:11 -0300

    fix bad commit

--- front-meli/src/App.js
+++ front-meli/src/App.js
@@ -1,10 +1,27 @@
-import Header from './components/Header'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Header from './components/Header';
+import SearchResult from './components/SearchResult'
+import ProductDetail from './components/ProductDetail'
+
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <main>
+      <Router>
+        <div>
+          <Header />
+        </div>
+        <Switch>
+          <Route exact path="/items" component={SearchResult}/>              
+          <Route exact path="/items/:id" component={ProductDetail}/>              
+        </Switch>
+      </Router>
+    </main>
   );
 }
 
--- front-meli/src/components/Header.jsx
+++ front-meli/src/components/Header.jsx
@@ -1,16 +1,57 @@
-import React from 'react'
-import '../css/header.css'
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../css/header.css';
+import SearchResult from '../components/SearchResult';
+import {
+    Link
+  } from "react-router-dom";
 
-const imgTag = process.env.PUBLIC_URL;
+const Header = () => {
+    
+    const imgTag = process.env.PUBLIC_URL;
 
-export const Header = () => {
+    //save search results
+    const [resultado, setResultado] = useState([]);
+
+    //Send request to enpoint
+    const getResults = async (input) => {
+        try {
+            const searchData = await axios.get(`http://localhost:9000/api/items?q=${input}`)
+            searchData ? setResultado(searchData.data) : setResultado(null);                 
+        } catch (err) {
+            console.error(err)
+        } 
+    }
+
+
+    //get data from input for search
+    const getQueryFromInput = (event) => {
+        const input = event[0].value;
+        getResults(input);
+    }
+//SOLUCIONAR ENVIO DE PROPS A VISTA SEARCH RESULT, LO INTENTE PERO NO LLEGA LA DATA, VER A FONDO
+    // console.log(resultado);
+
     return (
-        <div class="header">
-            <img class="meli-logo" src={imgTag + "/img/Logo_ML.png"} alt="meli-logo" />
-            <input class="search" type="text" placeholder="Nunca dejes de buscar"/>
-            <button type="submit" class="find" onClick>
-                <img src={imgTag + "/img/ic_Search.png"} alt=""/>
-            </button>
+        <div className="header">
+
+            <Link to="/">
+                <img className="meli-logo" src={imgTag + "/img/Logo_ML.png"} alt="meli-logo"/>        
+            </Link>
+
+            <form onSubmit={(ev) => { 
+                getQueryFromInput(document.getElementsByClassName('search'));
+                ev.preventDefault();
+                }
+            }>
+                <input className="search" type="text" placeholder="Nunca dejes de buscar"/>
+
+                <button type="submit" className="find">
+                    <img src={imgTag + "/img/ic_Search.png"} alt=""/>
+                </button>
+
+            </form>
+            
         </div>
     )
 }
